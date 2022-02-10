b63ed9fbe5c2432f5a6c3eed873e2511be025854
Ella Uppal <ellauppal@gmail.com>
2022-02-09 20:58:14 -0500

    Update UI

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,21 +4,21 @@ const Navbar = () => {
   function expandSidebar() {}
 
   return (
-    <nav>
-      <h1 className='nav-title'> EmpowerMe </h1>
-      <div className='nav-subheadings'>
-        <a onClick={() => history.push('/Blog')}> Blog </a>
-        <a onClick={() => history.push('/Empower')}>Empower </a>
-        <a> Sign up </a>
-        <button className='nav-sign-in'> Sign in </button>
-      </div>
-      <div className='sidebar'>
-        <a onClick={expandSidebar()}>
-          <i className='fa fa-bars'></i>
-        </a>
-      </div>
-    </nav>
-  );
-};
+    <nav1>
+        <h1 onClick={() => history.push('/')} className='nav1-title'> EmpowerMe </h1>
+        <div className='nav1-subheadings'>
+            <a1 onClick={() => history.push('/Blog')}> Blog </a1>
+            <a1 onClick={() => history.push('/Empower')}> Empower </a1>
+            <a1 onClick={() => history.push('/SignUp')} > Sign up </a1>
+            <button onClick={() => history.push('/SignIn')} className='nav-sign-in' > Sign in </button>
+        </div>
+        <div className="sidebar">
+            <a onClick={expandSidebar()}>
+                <i className="fa fa-bars"></i>
+            </a>
+        </div>
+    </nav1>
+)
+}
 
 export default Navbar;
--- src/components/Navbar2.jsx
+++ src/components/Navbar2.jsx
@@ -12,8 +12,8 @@ const Navbar2 = () => {
             <div className='nav2-subheadings'>
                 <a2 onClick={() => history.push('/Blog')}> Blog </a2>
                 <a2 onClick={() => history.push('/Empower')}> Empower </a2>
-                <a2> Sign up </a2>
-                <button className='nav-sign-in'> Sign out </button>
+                <a2 onClick={() => history.push('/SignUp')} > Sign up </a2>
+                <button className='nav-sign-in' > Sign out </button>
             </div>
             <div className="sidebar">
                 <a onClick={expandSidebar()}>
